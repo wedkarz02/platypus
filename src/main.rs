af4ab661b6f667f2c6e238be4e2005c3053f8a6a
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-12-07 14:02:23 +0100

    egui hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+struct Platypus {
+    msg: String,
+}
+
+impl Default for Platypus {
+    fn default() -> Self {
+        Self {
+            msg: String::from("Hello from Platypus"),
+        }
+    }
+}
+
+impl eframe::App for Platypus {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.centered_and_justified(|ui| {
+                ui.heading(&self.msg);
+            })
+        });
+    }
+}
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Platypus",
+        options,
+        Box::new(|_| Ok(Box::<Platypus>::default())),
+    )
 }
