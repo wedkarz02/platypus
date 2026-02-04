5e115fa3fe0af65051f61c1aea7dc2cf3737e5ca
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2026-02-04 20:01:54 +0100

    minimal eframe template example

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2026 Paweł Rybak
 
-use eframe::egui;
-
-struct Platypus {
-    msg: String,
-}
-
-impl Default for Platypus {
-    fn default() -> Self {
-        Self {
-            msg: String::from("Hello from Platypus"),
-        }
-    }
-}
-
-impl eframe::App for Platypus {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // ui.centered_and_justified(|ui| {
-            ui.heading(&self.msg);
-            // })
-        });
-    }
-}
+use platypus::gui;
 
 fn main() -> eframe::Result {
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+    env_logger::init(); // Log to stderr (run with RUST_LOG=debug)
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1024.0, 768.0])
+            .with_min_inner_size([640.0, 480.0]),
         ..Default::default()
     };
 
     eframe::run_native(
         "Platypus",
-        options,
-        Box::new(|_| Ok(Box::<Platypus>::default())),
+        native_options,
+        Box::new(|cc| Ok(Box::new(gui::App::new(cc)))),
     )
 }
